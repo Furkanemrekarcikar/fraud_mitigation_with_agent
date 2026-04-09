a2c1d394c453833613123b68fc5c3a547797c51a
Furkanemrekarcikar <168754834+Furkanemrekarcikar@users.noreply.github.com>
2026-04-10 00:53:44 +0300

    Update ml_tool.py

--- tools/ml_tool.py
+++ tools/ml_tool.py
@@ -13,13 +13,13 @@ def ml_score(txn: dict):
         # XGBoost modelinin eğitimde gördüğü ve tam olarak beklediği kolon isimlerini çekiyoruz
         expected_columns = model.get_booster().feature_names
 
-        # Modelin beklediği ama API'den gelmeyen tüm kolonları 0.0 ile dolduruyoruz
+        # Modelin beklediği ama APIden gelmeyen tüm kolonları 0la dolduruyoruz
         for col in expected_columns:
             if col not in df.columns:
                 df[col] = 0.0
 
-        # Veri setimizi tam olarak modelin beklediği sıraya ve kolonlara göre kırpıyoruz.
-        # Bu sayede modelin bilmediği 'user_id', 'merchant_id', 'country' gibi API parametreleri otomatik olarak dışarıda kalıyor.
+        # Veri setimni tam olarak modelin beklediği sıraya ve kolonlara göre kırp
+        # modelin bilmediği user_id merchant_id country gibi API parametreleri otomatik olarak dışarıda kaldı
         df_for_model = df[expected_columns]
 
         return float(model.predict_proba(df_for_model)[0][1])
